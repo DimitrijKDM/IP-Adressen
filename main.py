6b7f85fd31d4f1a7496cb037eee69d9daaf7a553
DimitrijKDM <127964621+DimitrijKDM@users.noreply.github.com>
2023-03-22 14:41:35 +0100

    Added Subnetmasc:
    Created classes for Binary and Decimal Subnetmasc, created Methods to define Decimal Subnetmasc and convert them to decimal

--- main.py
+++ main.py
@@ -45,6 +45,29 @@ class IPIDeeznutsDecimal:
         self.host_id_decimal = host_id_decimal
 
 
+class Subnetmasc:
+    def __init__(self, whole_subnetmasc, subnetmasc_1, subnetmasc_2, subnetmasc_a, subnetmasc_b, subnetmasc_c,
+                 subnetmasc_d, subnetmasc_readable):
+        self.subnetmasc_readable = subnetmasc_readable
+        self.subnetmasc_d = subnetmasc_d
+        self.subnetmasc_c = subnetmasc_c
+        self.subnetmasc_b = subnetmasc_b
+        self.subnetmasc_a = subnetmasc_a
+        self.subnetmasc_1 = subnetmasc_1
+        self.subnetmasc_2 = subnetmasc_2
+        self.whole_subnetmasc = whole_subnetmasc
+
+
+class SubnetmascDecimal:
+    def __init__(self, subnetmasc_decimal_a, subnetmasc_decimal_b, subnetmasc_decimal_c, subnetmasc_decimal_d,
+                 subnetmasc_decimal_readable):
+        self.subnetmasc_decimal_readable = subnetmasc_decimal_readable
+        self.subnetmasc_decimal_d = subnetmasc_decimal_d
+        self.subnetmasc_decimal_c = subnetmasc_decimal_c
+        self.subnetmasc_decimal_b = subnetmasc_decimal_b
+        self.subnetmasc_decimal_a = subnetmasc_decimal_a
+
+
 def define_ips():
     byte_array1 = os.urandom(1)
     byte_array2 = os.urandom(1)
@@ -96,7 +119,6 @@ def convert_binary_to_decimal(ip_address_decimal_32, netprefix):
 
 def convert_ip_address_to_decimal(ip_address):
     ip_address_decimal_a = 0
-    # Convert Sections into Decimals
     for digit in ip_address.ip_address_sectionA:
         ip_address_decimal_a = ip_address_decimal_a * 2 + int(digit)
     ip_address_decimal_b = 0
@@ -108,7 +130,6 @@ def convert_ip_address_to_decimal(ip_address):
     ip_address_decimal_d = 0
     for digit in ip_address.ip_address_sectionD:
         ip_address_decimal_d = ip_address_decimal_d * 2 + int(digit)
-    # Define Decimal IP Address (Put all Sections into one)
     ip_address_decimal_readable = f"{ip_address_decimal_a}.{ip_address_decimal_b}.{ip_address_decimal_c}." \
                                   f"{ip_address_decimal_d}"
     whole_ip_address_decimal = ip_address_decimal_a + ip_address_decimal_b + ip_address_decimal_c + ip_address_decimal_d
@@ -129,6 +150,42 @@ def ip_address_sections_always_eight_digits(ip_address_decimal):
     return ip_address_decimal_32
 
 
+def create_subnetmasc(netprefix):
+    whole_subnetmasc = "00000000000000000000000000000000"
+    subnetmasc_1 = whole_subnetmasc[:netprefix]
+    subnetmasc_2 = whole_subnetmasc[netprefix:]
+    subnetmasc_1 = subnetmasc_1.replace("0", "1")
+    whole_subnetmasc = subnetmasc_1 + subnetmasc_2
+    subnetmasc_a = whole_subnetmasc[:8]
+    subnetmasc_b = whole_subnetmasc[8:16]
+    subnetmasc_c = whole_subnetmasc[16:24]
+    subnetmasc_d = whole_subnetmasc[24:]
+    subnetmasc_readable = f"{subnetmasc_a}.{subnetmasc_b}.{subnetmasc_c}.{subnetmasc_d}"
+    subnetmasc = Subnetmasc(whole_subnetmasc, subnetmasc_1, subnetmasc_2, subnetmasc_a, subnetmasc_b, subnetmasc_c,
+                            subnetmasc_d, subnetmasc_readable)
+    return subnetmasc
+
+
+def convert_subnetmasc_to_decimal(subnetmasc):
+    subnetmasc_decimal_a = 0
+    for digit in subnetmasc.subnetmasc_a:
+        subnetmasc_decimal_a = subnetmasc_decimal_a * 2 + int(digit)
+    subnetmasc_decimal_b = 0
+    for digit in subnetmasc.subnetmasc_b:
+        subnetmasc_decimal_b = subnetmasc_decimal_b * 2 + int(digit)
+    subnetmasc_decimal_c = 0
+    for digit in subnetmasc.subnetmasc_c:
+        subnetmasc_decimal_c = subnetmasc_decimal_c * 2 + int(digit)
+    subnetmasc_decimal_d = 0
+    for digit in subnetmasc.subnetmasc_d:
+        subnetmasc_decimal_d = subnetmasc_decimal_d * 2 + int(digit)
+    subnetmasc_decimal_readable = f"{subnetmasc_decimal_a}.{subnetmasc_decimal_b}.{subnetmasc_decimal_c}." \
+                                  f"{subnetmasc_decimal_d}"
+    subnetmasc_decimal = SubnetmascDecimal(subnetmasc_decimal_a, subnetmasc_decimal_b, subnetmasc_decimal_c,
+                                           subnetmasc_decimal_d, subnetmasc_decimal_readable)
+    return subnetmasc_decimal
+
+
 def main():
     ip_address = define_ips()
     netprefix = ask_netprefix()
@@ -136,10 +193,13 @@ def main():
     ip_address_decimal = convert_ip_address_to_decimal(ip_address)
     ip_address_decimal_32 = ip_address_sections_always_eight_digits(ip_address_decimal)
     ip_ids_decimals = convert_binary_to_decimal(ip_address_decimal_32, netprefix)
+    subnetmasc = create_subnetmasc(netprefix)
+    subnetmasc_decimal = convert_subnetmasc_to_decimal(subnetmasc)
     print(f"IPv4: {ip_address_decimal.ip_address_decimal_readable}")
     print(f"Net-ID: {ip_ids_decimals.net_id_decimal.rstrip('.')}")
     print(f"Host-ID: {ip_ids_decimals.host_id_decimal.rstrip('.')}")
     print(f"Host Portion: {ip_ids.host_portion}")
+    print(f"Subnetmasc: {subnetmasc_decimal.subnetmasc_decimal_readable}")
 
 
 if __name__ == '__main__':
